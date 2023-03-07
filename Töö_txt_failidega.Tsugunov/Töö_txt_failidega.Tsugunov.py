90e77b8ec6b94759552f176a067f653d45cd808e
TsugunovS <stanislav.tsugunov@gmail.com>
2023-03-07 14:59:56 +0200

    Praktilinetöö_Sõnastik_

--- Töö_txt_failidega.Tsugunov/Töö_txt_failidega.Tsugunov.py
+++ Töö_txt_failidega.Tsugunov/Töö_txt_failidega.Tsugunov.py
@@ -1,8 +1,9 @@
 from module1 import *
-laused=["Tere tulemast"]
-
+laused=["Tere tulemast"] #
+text_est = Loe_failist('est.txt')
+text_rus = Loe_failist('rus.txt')
 while True:
-    v=int(input("1-Loeme failist\n2-Salvestame failisse\n3-Tekst helisse\n4-Tõlgi eesti või vene keelde\n"))
+    v=int(input("1-Loeme failist\n2-Salvestame failisse\n3-Tekst helisse\n4-Tõlgi eesti või vene keelde\n6-5 4 3"))
     if v==1:
         laused=Loe_failist("Laused.txt")
         for line in laused:
@@ -18,19 +19,31 @@ while True:
         #text : kõik elemendis järjendis
         ind=int(input("Number:"))
         Heli(laused[ind],'et')
+        
     elif v==4:
-        choice=input("1 Перевод с эстонского на русский.\n2 Перевод с русского на эстонский. ")
-    if choice == 1:
-        est_words = []
-        rus_words = []
-        with open("est.txt", "r") as file:
-            for line in file:
-                est, rus = line.strip().split(':')
-                est_words.append(est)
-                rus_words.append(rus)
-        est_word = input('Введите слово на эстонском: ')
+        est_word = input("Введите слово на эстонском: ")
         rus_word = est_to_rus(est_word, est_words, rus_words)
-            
+        if rus_word:
+            print("Перевод слова {est_word} на русский: {rus_word}")
+        else:
+            print(f"Слово {est_word} не найдено в словаре.")
+
+    elif v==5:
+        rus_words, est_words = load_dict("rus-est.txt")
+        rus_word = input("Введите слово на русском: ")
+        est_word = rus_to_est(rus_word, rus_words, est_words)
+        if est_word is not None:
+            print(f"Перевод слова {rus_word} на эстонский: {est_word}")
+        else:
+            print(f"Слово {rus_word} не найдено в словаре.")
+
+    elif v==6:
+        est = ["Tere", "Auto", "Õun"]
+        rus = ["Привет", "Машина", "Яблоко"]
+        hinne = Teadmiste_kontroll(rus, est)
+        print(est)
+        print(rus)
+        print(hinne)
             
 
 
--- Töö_txt_failidega.Tsugunov/module1.py
+++ Töö_txt_failidega.Tsugunov/module1.py
@@ -19,13 +19,61 @@ def Heli(text:str,keel:str):
     obj=gTTS(text=text,lang=keel,slow=False).save("heli.mp3")
     os.system("heli.mp3")
 
-def est_to_rus(word, est_words, rus_words):
-    with open('est.txt', 'r') as file:
+from random import * 
+def Teadmiste_kontroll(rus:list,est:list):
+    p=0
+    kokku = int(input("Mitu küsimust? "))
+    for i in range(kokku):
+        järjend=choice([rus,est])
+        sõna=choice(järjend)
+        print(f"{sõna} - ", end="")
+        tõlke=input()
+        if sõna in rus:
+            i=rus.index(sõna)
+            tõlke_kontroll=est[i]
+        elif sõna in est:
+            i=est.index(sõna)
+            tõlke_kontroll=rus[i]
+        if tõlke==tõlke_kontroll:
+            p+=1
+            print("Õige")
+        else:
+            print("Vale")
+    if (p/kokku)*100>90:
+        hinne=5
+    elif (p/kokku)*100>75:
+        hinne=4
+    elif (p/kokku)*100>60: 
+        hinne=3
+    else:
+        hinne="Väga halb!"
+    return hinne
+
+def load_dict(file_name):
+    words1 = []
+    words2 = []
+    with open(file_name, 'r') as file:
         for line in file:
-            est, rus = line.strip().split(':')
-            if word == est:
-                return rus
-    return None
+            word1, word2 = line.strip().split(':')
+            words1.append(word1)
+            words2.append(word2)
+    return words1, words2
+
+
+def est_to_rus(word, est_words, rus_words):
+    try:
+        index = est_words.index(word)
+        return rus_words[index]
+    except print("Vale!"):
+        return None 
+
+def rus_to_est(word, rus_words, est_words):
+    try:
+        index = rus_words.index(word)
+        return est_words[index]
+    except print("Vale!"):
+        return None
+
 
 
 
